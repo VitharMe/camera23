f794f6955f73ec5880dee9300688d91cc3f5a5ba
Vithar Me <vithar@vithar.me>
2021-01-21 21:49:24 +0100

    Update cam.py

--- cam.py
+++ cam.py
@@ -38,33 +38,35 @@ atexit.register(camera.close)
 camera.resolution = sizeData[sizeMode][1]
 camera.crop       = (0.0, 0.0, 1.0, 1.0)
 camera.rotation = 270
-
-while(True):
-  button_23 = GPIO.input(23)
-  event = pygame.event.poll()
-  stream = io.BytesIO()
-  camera.capture(stream, use_video_port=True, format='raw')
-  stream.seek(0)
-  stream.readinto(yuv)
-  stream.close()
-  yuv2rgb.convert(yuv, rgb, sizeData[sizeMode][1][0],
-    sizeData[sizeMode][1][1])
-  img = pygame.image.frombuffer(rgb[0:
-    (sizeData[sizeMode][1][0] * sizeData[sizeMode][1][1] * 3)],
-    sizeData[sizeMode][1], 'RGB')
-  if img:
-    screen.blit(img,
-      ((320 - img.get_width() ) / 2,
-       (240 - img.get_height()) / 2))
-  if event.type == pygame.MOUSEBUTTONDOWN:
-    pygame.draw.rect(screen,(255,255,255), (0,0,320,240))
-    pygame.display.update()
-    now = datetime.datetime.now()
-    image_date = now.strftime("%Y-%m-%d_%H%M%S.jpg")
-    image_path = os.path.join(current_path, image_date)
-    camera.capture(image_path)
-    img = pygame.image.load(image_path)
-    screen.blit(img, (0,0))
-    pygame.display.update()
-    time.sleep(1)
-  pygame.display.update()
+def deploy():
+    while(True):
+      button_23 = GPIO.input(23)
+      event = pygame.event.poll()
+      stream = io.BytesIO()
+      camera.capture(stream, use_video_port=True, format='raw')
+      stream.seek(0)
+      stream.readinto(yuv)
+      stream.close()
+      yuv2rgb.convert(yuv, rgb, sizeData[sizeMode][1][0],
+        sizeData[sizeMode][1][1])
+      img = pygame.image.frombuffer(rgb[0:
+        (sizeData[sizeMode][1][0] * sizeData[sizeMode][1][1] * 3)],
+        sizeData[sizeMode][1], 'RGB')
+      if img:
+        screen.blit(img,
+          ((320 - img.get_width() ) / 2,
+           (240 - img.get_height()) / 2))
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        pygame.draw.rect(screen,(255,255,255), (0,0,320,240))
+        pygame.display.update()
+        now = datetime.datetime.now()
+        image_date = now.strftime("%Y-%m-%d_%H%M%S.jpg")
+        image_path = os.path.join(current_path, image_date)
+        camera.capture(image_path)
+        img = pygame.image.load(image_path)
+        screen.blit(img, (0,0))
+        pygame.display.update()
+        time.sleep(1)
+      pygame.display.update()
+if __name__ == '__main__':
+    deploy()
