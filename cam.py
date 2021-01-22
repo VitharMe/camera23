cdadc755845981d90d828e53e829500300bd4e6e
Vithar Me <vithar@vithar.me>
2021-01-22 17:04:38 +0100

    Cross

--- cam.py
+++ cam.py
@@ -30,7 +30,8 @@ rgb = bytearray(320 * 240 * 3)
 yuv = bytearray(320 * 240 * 3 / 2)
 
 pygame.init()
-pygame.mouse.set_visible(False)
+pitft = pigame.PiTft()
+pygame.mouse.set_visible(True)
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 
 camera            = picamera.PiCamera()
@@ -41,35 +42,45 @@ camera.rotation = 270
 
 def deploy():
     while(True):
-      button_23 = GPIO.input(23)
-      event = pygame.event.poll()
-      stream = io.BytesIO()
-      camera.capture(stream, use_video_port=True, format='raw')
-      stream.seek(0)
-      stream.readinto(yuv)
-      stream.close()
-      yuv2rgb.convert(yuv, rgb, sizeData[sizeMode][1][0],
-        sizeData[sizeMode][1][1])
-      img = pygame.image.frombuffer(rgb[0:
-        (sizeData[sizeMode][1][0] * sizeData[sizeMode][1][1] * 3)],
-        sizeData[sizeMode][1], 'RGB')
-      if img:
-        screen.blit(img,
-          ((320 - img.get_width() ) / 2,
-           (240 - img.get_height()) / 2))
-      if event.type == pygame.MOUSEBUTTONDOWN:
-        pygame.draw.rect(screen,(255,255,255), (0,0,320,240))
+        pitft.update()
+        event = pygame.event.poll()
+        stream = io.BytesIO()
+        camera.capture(stream, use_video_port=True, format='raw')
+        stream.seek(0)
+        stream.readinto(yuv)
+        stream.close()
+        yuv2rgb.convert(yuv, rgb, sizeData[sizeMode][1][0],
+            sizeData[sizeMode][1][1])
+        img = pygame.image.frombuffer(rgb[0:
+         (sizeData[sizeMode][1][0] * sizeData[sizeMode][1][1] * 3)],
+          sizeData[sizeMode][1], 'RGB')
+        if img:
+            screen.blit(img,
+              ((320 - img.get_width() ) / 2,
+               (240 - img.get_height()) / 2))        
+        for event in pygame.event.get():
+            if(event.type is MOUSEBUTTONDOWN):
+                x,y = pygame.mouse.get_pos()
+            elif(event.type is MOUSEBUTTONUP):
+                x,y = pygame.mouse.get_pos()
+                if y > 200:
+                            if x < 50:
+                                    print("Cross")
+                            else:
+                                    print("Photo")
+                                    pygame.draw.rect(screen,(255,255,255), (0,0,320,240))
+                                    pygame.display.update()
+                                    now = datetime.datetime.now()
+                                    image_date = now.strftime("pics/%Y-%m-%d_%H%M%S.jpg")
+                                    image_path = os.path.join(current_path, image_date)
+                                    camera.capture(image_path)
+                                    img = pygame.image.load(image_path)
+                                    screen.blit(img, (0,0))
+                                    pygame.display.update()
+                                    time.sleep(1)
+        img_cross = pygame.image.load('resources/cross.png')
+        screen.blit(img_cross, (280,5))
         pygame.display.update()
-        now = datetime.datetime.now()
-        image_date = now.strftime("pics/%Y-%m-%d_%H%M%S.jpg")
-        image_path = os.path.join(current_path, image_date)
-        camera.capture(image_path)
-        img = pygame.image.load(image_path)
-        screen.blit(img, (0,0))
-        pygame.display.update()
-        time.sleep(1)
-      img_cross = pygame.image.load('resources/cross.png')
-      screen.blit(img_cross, (280,5))
-      pygame.display.update()
+        sleep(0.1)
 if __name__ == '__main__':
     deploy()
